8ac5966bbd395b3c792ac8126275abb5a56baeff
Yasuaki Goto <yasu1003@gmail.com>
2015-02-09 14:01:12 +0900

    リダイレクトのパスが違うんじゃない？？

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
     def create
         @post = Post.new(post_params)
         @post.save
-        redirect_to @post
+        redirect_to root_path
     end
     def show
         @post = Post.find(params[:id])
